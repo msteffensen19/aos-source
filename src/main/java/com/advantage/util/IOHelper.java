b3d1f378898b8736ca69f374ebf274e3c5e0b15c
dalie <eli.dali@hpe.com>
2015-10-25 15:17:46 +0200

    user story #1251: Special Offers Service

--- src/main/java/com/advantage/util/IOHelper.java
+++ src/main/java/com/advantage/util/IOHelper.java
@@ -12,24 +12,40 @@ public abstract class IOHelper {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Get the content of the file in the given file path, as a byte array.
+     * @param filePath the path of the file to get it's content as a byte array.
+     * @return the content of the file in the given file path, as a byte array.
+     * @throws IOException if an I/O error occurs.
+     * @throws IllegalArgumentException if the given file path argument references <b>null</b>, or if it <b>is</b> a
+     * blank string.
+     */
     public static byte[] fileContentToByteArray(final String filePath) throws IOException {
 
+        ArgumentValidationHelper.validateStringArgumentIsNotNullAndNotBlank(filePath, "file path");
         final InputStream in = new FileInputStream(filePath);
         final ByteArrayOutputStream out = new ByteArrayOutputStream();
         IOHelper.outputInput(in, out);
         return out.toByteArray();
     }
 
+    /**
+     * Read from the given input and write the content that was read to the given output stream.
+     * @param in the input stream to read from.
+     * @param out the output stream to write to.
+     * @throws IOException if an I/O error occurs.
+     * @throws IllegalArgumentException if any one of the arguments references <b>null</b>.
+     */
     public static void outputInput(final InputStream in, final OutputStream out) throws IOException {
 
         ArgumentValidationHelper.validateArgumentIsNotNull(in, "input stream");
         ArgumentValidationHelper.validateArgumentIsNotNull(out, "output stream");
         final byte[] buffer = new byte[512];
-        int bytesReadcount;
+        int numberOfBytesRead;
 
-        while ((bytesReadcount = in.read(buffer)) != -1) {
+        while ((numberOfBytesRead = in.read(buffer)) != -1) {
 
-            out.write(buffer, 0, bytesReadcount);
+            out.write(buffer, 0, numberOfBytesRead);
         }
     }
 }
