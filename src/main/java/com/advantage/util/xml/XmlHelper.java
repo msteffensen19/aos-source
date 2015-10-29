6afcbca6375b1c9aea421030a9e0603dd6b1981b
dalie <eli.dali@hpe.com>
2015-10-29 17:24:10 +0200

    user story #1154: Define REST API for catalog serivce
    Task-Url:
    https://agilemanager-lon.saas.hp.com/agm/webui/alm/t578277652_hp_com/main/apm/#product/backlog_items/shared.update;entityTypeName=requirement;entityId=1154

--- src/main/java/com/advantage/util/xml/XmlHelper.java
+++ src/main/java/com/advantage/util/xml/XmlHelper.java
@@ -1,12 +1,21 @@
 package com.advantage.util.xml;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
@@ -26,18 +35,32 @@ public abstract class XmlHelper {
 		throw new UnsupportedOperationException();
 	}
 
+	/**
+	 * Get a document from the file in the given path.
+	 * @param filePath the path of the file, to a document from.
+	 * @return a document from the file in the given path.
+	 * @throws IOException if an I/O error occurs.
+	 * @throws IllegalArgumentException if the given file path argument references
+	 * <b>null</b>, or if it <b>is</b> a blank string.
+	 */
 	public static Document getDocument(final String filePath)
-	 throws IOException, ParserConfigurationException, SAXException {
+	 throws IOException {
 
 		ArgumentValidationHelper.validateStringArgumentIsNotNullAndNotBlank(filePath,
 				                                                            "file path");
-		final DocumentBuilder documentBuilder = XmlHelper.getDocumentBuilder();
 		InputStream in = null;
 
 		try {
 
+			final DocumentBuilder documentBuilder = XmlHelper.getDocumentBuilder();
 			in = new FileInputStream(filePath);
 			return documentBuilder.parse(in);
+		} catch (final ParserConfigurationException ex) {
+
+			throw new RuntimeException(ex);
+		} catch (final SAXException ex) {
+
+			throw new RuntimeException(ex);
 		} finally {
 
 			IOHelper.closeInputStreamIfNotNull(in);
@@ -54,5 +77,30 @@ public abstract class XmlHelper {
 
 		final DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
 		return documentBuilderFactory.newDocumentBuilder();
+	}
+
+	public static void persistDocument(final Document document, final String filePath)
+	 throws IOException {
+
+		ArgumentValidationHelper.validateArgumentIsNotNull(document, "document");
+		ArgumentValidationHelper.validateStringArgumentIsNotNullAndNotBlank(filePath,
+                                                                            "file path");
+		final TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		OutputStream out = null;
+
+		try {
+
+			final Transformer transformer = transformerFactory.newTransformer();
+			final Source inputSource = new DOMSource(document.getDocumentElement());
+			out = new FileOutputStream(filePath);
+			final Result result = new StreamResult(out);
+			transformer.transform(inputSource, result);
+		} catch (final TransformerException ex) {
+
+			throw new RuntimeException(ex);
+		} finally {
+
+			IOHelper.closeOutputStreamIfNotNull(out);
+		}
 	}
 }
