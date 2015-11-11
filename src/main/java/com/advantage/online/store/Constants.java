a589b9d1dfff1b989f2b57548c00a4924c049828
regevb@hpe.com <regevb@hpe.com>
2015-11-11 13:37:26 +0200

    Deal improvements

--- src/main/java/com/advantage/online/store/Constants.java
+++ src/main/java/com/advantage/online/store/Constants.java
@@ -1,8 +1,21 @@
 package com.advantage.online.store;
 
 public class Constants {
+	/**
+	 * Keyboard special characters
+	 */
+	public static final char COLLON = ':'; 
+	
+	public static final char DASH = '-'; 
+	
+	public static final char SEMI_COLLON = ';'; 
 
-    /**
+	public static final char SLASH = '/'; 
+
+	public static final char SPACE = ' '; 
+	
+
+	/**
      * prefix of REST API
      */
     public static final String URI_API = "/api";
