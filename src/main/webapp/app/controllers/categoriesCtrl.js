6c27d88776b3bd28de653f3298c1dc821d5248fe
Kubany, Assaf <kubany@hpe.com>
2015-11-18 10:18:11 +0200

    layout changes, UI refactoring

--- src/main/webapp/app/controllers/categoriesCtrl.js
+++ src/main/webapp/app/controllers/categoriesCtrl.js
@@ -39,7 +39,10 @@ define(['./module'], function (controllers) {
             };
 
             $scope.uri = "http://localhost:8080/";
-            $scope.images = [{imageName : 'Banner1.jpg', imageId : 0},{imageName : 'Banner2.jpg', imageId : 1},{imageName : 'Banner3.jpg', imageId : 2}];
+            $scope.images = [
+                {imageName : 'Banner1.jpg', imageId : 0},
+                {imageName : 'Banner2.jpg', imageId : 1},
+                {imageName : 'Banner3.jpg', imageId : 2}];
 
         }]);
 });
