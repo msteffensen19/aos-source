242c78e01d41a80ad2f5871bf361c63a562f0c1e
dalie <eli.dali@hpe.com>
2015-11-18 10:36:47 +0200

    user story #1268: API for Infrastructure for category products fetch

--- src/main/java/com/advantage/online/store/config/ImageManagementConfiguration.java
+++ src/main/java/com/advantage/online/store/config/ImageManagementConfiguration.java
@@ -10,19 +10,34 @@ import org.springframework.core.env.Environment;
 import com.advantage.online.store.image.ImageManagement;
 import com.advantage.online.store.image.ImageManagementAccess;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 @Configuration
 @PropertySources(value = {@PropertySource("classpath:imageManagement.properties")})
 public class ImageManagementConfiguration {
 
-	public static final String PROPERTY_IMAGE_MANAGEMENT_REPOSITORY = "advantage.imageManagement.repository";
+    public static final String PROPERTY_IMAGE_MANAGEMENT_REPOSITORY = "advantage.imageManagement.repository";
 
-	@Autowired
-	private Environment environment;
+    @Autowired
+    private Environment environment;
 
-	@Bean(name = "imageManagement")
-	public ImageManagement getImageManagement() {
+    @Bean(name = "imageManagement")
+    public ImageManagement getImageManagement() throws IOException {
 
-		final String imageManagementRepository = environment.getProperty(ImageManagementConfiguration.PROPERTY_IMAGE_MANAGEMENT_REPOSITORY);
-		return ImageManagementAccess.getImageManagement(imageManagementRepository);
-	}
+        final String imageManagementRepository = environment.getProperty(ImageManagementConfiguration.PROPERTY_IMAGE_MANAGEMENT_REPOSITORY);
+        return ImageManagementAccess.getImageManagement(getPath() + imageManagementRepository);
+    }
+
+    private String getPath() throws UnsupportedEncodingException {
+        String path = this.getClass().getClassLoader().getResource("").getPath();
+        String fullPath = URLDecoder.decode(path, "UTF-8");
+        //String pathArr[] = fullPath.split("/WEB-INF/classes/");
+        String pathArr[] = fullPath.split("/target/");
+        fullPath = pathArr[0];
+
+        return fullPath.substring(1);
+    }
 }
