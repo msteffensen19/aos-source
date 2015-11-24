7312f56dcfd49b75eef65f02f175a965fb07afb4
correnti <efi.correnti@hpe.com>
2015-11-24 10:19:40 +0200

    user story #1280: As a user i would like to click login using my credentials

---
+++ src/main/webapp/utils/Models/UserCookie.js
@@ -0,0 +1,12 @@
+/**
+ * Created by correnti on 20/11/2015.
+ */
+
+var userCookie = userCookie || {};
+
+userCookie.fillParams = function(name, id, email, date){
+      this.name = name;
+      this.id = id;
+      this.email = email;
+      this.date = date;
+}
