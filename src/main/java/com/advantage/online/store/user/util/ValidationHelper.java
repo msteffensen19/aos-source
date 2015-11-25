f0c81c26c7954ec3dc9c5eae14bf358f69185de3
regevb@hpe.com <regevb@hpe.com>
2015-11-25 12:16:50 +0200

    User Management Implementation - Password validation compliant with AOS policy and Phone number validation including international country-code

--- src/main/java/com/advantage/online/store/user/util/ValidationHelper.java
+++ src/main/java/com/advantage/online/store/user/util/ValidationHelper.java
@@ -1,9 +1,7 @@
 package com.advantage.online.store.user.util;
 
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -12,13 +10,13 @@ import java.util.regex.Pattern;
 public class ValidationHelper {
 
     //    private static final String PHONE_PATTERN = "^\\(?([0-9]{3})\\)?[-.\\s]?([0-9]{3})[-.\\s]?([0-9]{4})$";
-    private static final String PHONE_PATTERN = "^\\+(([0-9]{1,3}))?[-.\\s]\\(?([0-9]{1,3})\\)?[-.\\s]?([0-9]{3})[-.\\s]?([0-9]{4})$";
+    private static final String PHONE_PATTERN = "^\\+([0-9]{1,3})?[-.\\s]\\(?([0-9]{1,3})\\)?[-.\\s]?([0-9]{3})[-.\\s]?([0-9]{4})$";
 
     private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" +
                                                 "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
     //  Contain 3-15 characters of: Digits 0-9, UPPER case (A-Z), lower case (a-z), underscore (_) and hyphen (-)
-    private static final String LOGIN_PATTERN = "^[A-Za-z0-9_.-]{3,15}$";
+    private static final String LOGIN_USER_NAME_PATTERN = "^[A-Za-z0-9_.-]{3,15}$";
 
     /**
      * <ul><code>Password</code> must be compliant with <b>AOS policy</b></ul>:
@@ -26,12 +24,10 @@ public class ValidationHelper {
      * Password must contain one or more numerical digits and
      * both UPPER-CASE and lower-case characters (case sensitivity)
      */
-    //private static final String PASSWORD_PATTERN = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%\\.\\-\\+\\*]).{6,20})";
-    private static final String PASSWORD_PATTERN = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{5,10})";
+    //private static final String LOGIN_PASSWORD_PATTERN = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%\\.\\-\\+\\*]).{6,20})";
+    private static final String LOGIN_PASSWORD_PATTERN = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{5,10})";
 
     private static final String TIME_24HOURS_PATTERN = "([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]";
-
-    private static final String DATE_YYYYMMDD_PATTERN = "((19|20)\\d\\d)-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])";
 
     //  EUROPEAN_DATE_FORMAT = "dd.MM.yyyy" ; AMERICAN_DATE_FORMAT = "MM/dd/yyyy" ; SCANDINAVIAN_DATE_FORMAT = "yyyy-MM-dd"
     private static final String AMERICAN_DATE_PATTERN = "(0?[1-9]|1[012])/(0?[1-9]|[12][0-9]|3[01])/((19|20)\\d\\d)";
@@ -40,70 +36,73 @@ public class ValidationHelper {
 
     private static Pattern pattern;
 
-    private static Matcher matcher;
-
     public ValidationHelper() {
 
     }
 
-    public static boolean isValidEmail(final String email) {
+    /**
+     * Checks that {@code userName} is a valid user-name and in compliance with AOS policy.
+     * @param userName {@link String} to verify as a valid user-name for login.
+     * @return <b>true</b> when {@code userName} is valid, otherwise <b>false</b>.
+     */
+    public static boolean isValidLogin(final String userName) {
+        pattern = Pattern.compile(LOGIN_USER_NAME_PATTERN);
 
-        pattern = Pattern.compile(EMAIL_PATTERN);
-
-        matcher = pattern.matcher(email);
-
-        final boolean isValid = matcher.matches();
-        System.out.println(email +" : "+ isValid);
-
-        return isValid;
-
-    }
-
-    public static boolean isValidLogin(final String login) {
-
-        pattern = Pattern.compile(LOGIN_PATTERN);
-
-        matcher = pattern.matcher(login);
-
-        final boolean isValid = matcher.matches();
-        System.out.println(login +" : "+ isValid);
+        final boolean isValid = pattern.matcher(userName).matches();
+        System.out.println(userName +" : "+ isValid);
 
         return isValid;
 
     }
 
     /**
-     * Checks if <code>password</code> complies with AOS policy as a valid password.
-     * @param password {@link String} to check for valid time format.
-     * @return <code>true</code> when <code>time24h</code> is a valid time format,
-     * otherwise <code>false</code>.
+     * Checks that {@code password} is a valid password and in compliance AOS policy.
+     * @param password {@link String} to verify as a password for login.
+     * @return <b>true</b> when {@code password} is valid, otherwise <b>false</b>.
      */
     public static boolean isValidPassword(final String password) {
+        pattern = Pattern.compile(LOGIN_PASSWORD_PATTERN);
 
-        pattern = Pattern.compile(PASSWORD_PATTERN);
-
-        matcher = pattern.matcher(password);
-
-        final boolean isValid = matcher.matches();
+        final boolean isValid = pattern.matcher(password).matches();
         System.out.println(password +" : "+ isValid);
 
         return isValid;
 
     }
 
+    public static boolean isValidPhoneNumber(final String phoneNumber) {
+        pattern = Pattern.compile(PHONE_PATTERN);
+
+        final boolean isValid = pattern.matcher(phoneNumber).matches();
+        System.out.println(phoneNumber +" : "+ isValid);
+
+        return isValid;
+    }
+
     /**
-     * Check that <code>time24h</code> is a valid 24-hours time format.
-     * @param time24h {@link String} to check for valid time format.
-     * @return <code>true</code> when <code>time24h</code> is a valid time format,
-     * otherwise <code>false</code>.
+     * Check that {@code e-mail} is a valid e-mail address.
+     * @param email {@link String} to verify as a valid e-mail address.
+     * @return <b>true</b> when {@code e-mail} is valid, otherwise <b>false</b>.
+     */
+    public static boolean isValidEmail(final String email) {
+        pattern = Pattern.compile(EMAIL_PATTERN);
+
+        final boolean isValid = pattern.matcher(email).matches();
+        System.out.println(email +" : "+ isValid);
+
+        return isValid;
+
+    }
+
+    /**
+     * Check that {@code time24h} is a valid 24-hours time format.
+     * @param time24h {@link String} to verify as a having a valid 24-hours time format.
+     * @return <b>true</b> when {@code time24h} is a valid time format, otherwise <b>false</b>.
      */
     public static boolean isValidTime24h(final String time24h) {
-
         pattern = Pattern.compile(TIME_24HOURS_PATTERN);
 
-        matcher = pattern.matcher(time24h);
-
-        final boolean isValid = matcher.matches();
+        final boolean isValid = pattern.matcher(time24h).matches();
         System.out.println(time24h +" : "+ isValid);
 
         return isValid;
@@ -111,12 +110,10 @@ public class ValidationHelper {
     }
 
     /**
-     * Check that <code>stringDate</code> is a valid date format, either
-     * EUROPEAN, AMERICAN or SCANDINAVIAN.
+     * Check that {@code stringDate} is a valid date format, either EUROPEAN, AMERICAN or SCANDINAVIAN.
      * <br/>
-     * @param stringDate {@link String} to check for valid date format.
-     * @return <code>true</code> when <code>stringDate</code> is a valid date format,
-     * otherwise <code>false</code>.
+     * @param stringDate {@link String} verify as having a valid date format.
+     * @return <b>true</b> when {@code stringDate} is a valid date format, otherwise <b>false</b>.
      */
     public static boolean isValidDate(final String stringDate) {
         SimpleDateFormat dateFormat;
@@ -125,9 +122,11 @@ public class ValidationHelper {
             dateFormat = new SimpleDateFormat("MM/dd/yyyy");
         } else if (Pattern.compile(EUROPEAN_DATE_PATTERN).matcher(stringDate).matches()) {
             dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        } else {
-            // (Pattern.compile(SCANDINAVIAN_DATE_PATTERN).matcher(stringDate).matches())
+        } else if (Pattern.compile(SCANDINAVIAN_DATE_PATTERN).matcher(stringDate).matches()) {
             dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        } else {
+            //  invalid date format
+            return false;
         }
 
         dateFormat.setLenient(false);
@@ -146,18 +145,25 @@ public class ValidationHelper {
     public static void main(String[] args) {
         //ValidationHelper validationHelper = new ValidationHelper();
 
-        ValidationHelper.isValidDate("29.02.2012");
-        ValidationHelper.isValidDate("29.02.2011");
+        ValidationHelper.isValidDate("29.02.2012"); //  valid EUROPEAN date-format
+        ValidationHelper.isValidDate("29.02.2011"); //  invalid EUROPEAN date-format: no Feb 29th in 2011
 
-        ValidationHelper.isValidDate("02/29/2012");
-        ValidationHelper.isValidDate("02/29/2011");
+        ValidationHelper.isValidDate("02/29/2012"); //  valid AMERICAN date-format
+        ValidationHelper.isValidDate("02/29/2011"); //  invalid AMERICAN date-format: no Feb 29th in 2011
 
-        ValidationHelper.isValidDate("2012-02-29");
-        ValidationHelper.isValidDate("2011-02-29");
+        ValidationHelper.isValidDate("2012-02-29"); //  valid SCANDINAVIAN date-format
+        ValidationHelper.isValidDate("2011-02-29"); //  invalid SCANDINAVIAN date-format: no Feb 29th in 2011
 
-        ValidationHelper.isValidEmail("a@b.com");
         ValidationHelper.isValidLogin("king.david");
+        ValidationHelper.isValidEmail("a@b.com");
         ValidationHelper.isValidTime24h("23:59:60");
+
+        ValidationHelper.isValidPhoneNumber("+1 123 456 7890");
+        ValidationHelper.isValidPhoneNumber("+972 54 123 4567");
+        ValidationHelper.isValidPhoneNumber("+972 54 1234567");
+        ValidationHelper.isValidPhoneNumber("+44 123 4567890");
+        ValidationHelper.isValidPhoneNumber("+44 1234567890");
+        ValidationHelper.isValidPhoneNumber("+441234567890");
 
         ValidationHelper.isValidPassword("King1david");
         ValidationHelper.isValidPassword("king2David");
