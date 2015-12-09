5702611f5289bbdd7787235b1399f6e6c7e17191
Evgeney Fiskin <evgeney.fiskin@hpe.com>
2015-12-09 19:51:35 +0200

    Service refactoring:
    Move AppUserConfigurationController to ServiceController

--- src/main/java/com/advantage/online/store/api/ServiceController.java
+++ src/main/java/com/advantage/online/store/api/ServiceController.java
@@ -1,30 +1,24 @@
 package com.advantage.online.store.api;
 
 import com.advantage.online.store.Constants;
-import com.advantage.online.store.dao.product.ProductRepository;
 import com.advantage.online.store.dao.category.CategoryRepository;
-import com.advantage.online.store.dto.*;
+import com.advantage.online.store.dao.product.ProductRepository;
 import com.advantage.online.store.log.AppUserAuthorize;
 import com.advantage.online.store.model.deal.Deal;
-import com.advantage.online.store.model.attribute.Attribute;
-import com.advantage.online.store.model.category.Category;
-import com.advantage.online.store.model.product.Product;
-import com.advantage.online.store.model.product.ProductAttributes;
 import com.advantage.online.store.services.AttributeService;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+import com.advantage.online.store.user.dto.AppUserConfigurationResponseStatus;
+import com.advantage.online.store.user.services.AppUserConfigurationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityManagerFactory;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping(value = Constants.URI_API + "/service")
@@ -41,62 +35,73 @@ public class ServiceController {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private AppUserConfigurationService appUserConfigurationService;
 
-   /* @RequestMapping(value = "/initdb", method = RequestMethod.POST)
-    public Boolean initDb(@RequestBody CategoryDto dto) {
-        try {
-            Category category = categoryRepository.get(dto.getCategoryId());
-            SessionFactory sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
+//    @RequestMapping(value = "/initdb", method = RequestMethod.POST)
+//    public Boolean initDb(@RequestBody CategoryDto dto) {
+//        try {
+//            Category category = categoryRepository.get(dto.getCategoryId());
+//            SessionFactory sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
+//
+//            Session session = sessionFactory.openSession();
+//            Transaction transaction = session.beginTransaction();
+//
+//            Map<Long, Product> productMap = new HashMap<>();
+//            List<Product> productList = productRepository.getAll();
+//            for (Product product : productList) {
+//                productMap.put(product.getId(), product);
+//            }
+//
+//            Map<String, Attribute> defAttributes = new HashMap<>();
+//
+//            List<Attribute> attributeList = attributeService.getAllAttributes();
+//            for (Attribute attribute : attributeList) {
+//                defAttributes.put(attribute.getName().toUpperCase(), attribute);
+//            }
+//
+//            PRODUCT
+//            for (ProductDto p : dto.getProducts()) {
+//                Product product = new Product(p.getProductName(), p.getDescription(), p.getPrice(), category);
+//                product.setManagedImageId(p.getImageUrl());
+//                session.persist(product);
+//                //load attributes
+//                for (AttributeItem a : p.getAttributes()) {
+//                    ProductAttributes attributes = new ProductAttributes();
+//                    attributes.setProduct(product);
+//                    attributes.setAttribute(defAttributes.get(a.getAttributeName().toUpperCase()));
+//                    attributes.setAttributeValue(a.getAttributeValue());
+//
+//                    session.save(attributes);
+//                }
+//
+//                productMap.put(product.getId(), product);
+//            }
+//
+//            PromotedProductDto p = dto.getPromotedProduct();
+//            Product parent = productMap.get(p.getId());
+//
+//            Deal deal = new Deal(10, parent.getDescription(), p.getPromotionHeader(), p.getPromotionSubHeader(), p.getStaringPrice(),
+//                p.getPromotionImageId(), 0, "", "",  parent);
+//
+//            session.persist(deal);
+//            transaction.commit();
+//
+//            return true;
+//        }
+//        catch (Exception e) {
+//            return false;
+//        }
+//    }
 
-            Session session = sessionFactory.openSession();
-            Transaction transaction = session.beginTransaction();
+    @RequestMapping(value = "/clientConfiguration", method = RequestMethod.GET)
+    public ResponseEntity<AppUserConfigurationResponseStatus> getAllConfigurationParameters() {
 
-            Map<Long, Product> productMap = new HashMap<>();
-            List<Product> productList = productRepository.getAll();
-            for (Product product : productList) {
-                productMap.put(product.getId(), product);
-            }
+        AppUserConfigurationResponseStatus appUserConfigurationResponseStatus = appUserConfigurationService.getAllConfigurationParameters();
 
-            Map<String, Attribute> defAttributes = new HashMap<>();
+        return new ResponseEntity<>(appUserConfigurationResponseStatus, (appUserConfigurationResponseStatus != null ? HttpStatus.OK : HttpStatus.NOT_FOUND));
 
-            List<Attribute> attributeList = attributeService.getAllAttributes();
-            for (Attribute attribute : attributeList) {
-                defAttributes.put(attribute.getName().toUpperCase(), attribute);
-            }
-
-            *//*PRODUCT*//*
-            for (ProductDto p : dto.getProducts()) {
-                Product product = new Product(p.getProductName(), p.getDescription(), p.getPrice(), category);
-                product.setManagedImageId(p.getImageUrl());
-                session.persist(product);
-                //load attributes
-                for (AttributeItem a : p.getAttributes()) {
-                    ProductAttributes attributes = new ProductAttributes();
-                    attributes.setProduct(product);
-                    attributes.setAttribute(defAttributes.get(a.getAttributeName().toUpperCase()));
-                    attributes.setAttributeValue(a.getAttributeValue());
-
-                    session.save(attributes);
-                }
-
-                productMap.put(product.getId(), product);
-            }
-
-            PromotedProductDto p = dto.getPromotedProduct();
-            Product parent = productMap.get(p.getId());
-
-            Deal deal = new Deal(10, parent.getDescription(), p.getPromotionHeader(), p.getPromotionSubHeader(), p.getStaringPrice(),
-                p.getPromotionImageId(), 0, "", "",  parent);
-
-            session.persist(deal);
-            transaction.commit();
-
-            return true;
-        }
-        catch (Exception e) {
-            return false;
-        }
-    }*/
+    }
 
     @RequestMapping(value = "/tests/auth_method", method = RequestMethod.GET)
     @AppUserAuthorize
