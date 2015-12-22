002b9fed659fd4929b51d34c7c2985af0a0efdd4
regevb@hpe.com <regevb@hpe.com>
2015-12-22 10:06:35 +0200

    MasterCredit service MOCK

--- mastercredit/src/main/java/advantage/online/store/dto/MasterCreditDto.java
+++ mastercredit/src/main/java/advantage/online/store/dto/MasterCreditDto.java
@@ -2,18 +2,6 @@ package advantage.online.store.dto;
 
 public class MasterCreditDto {
 
-    private class ReceivingCard {
-        private long accountNumber;     //  fixed 12 digits
-
-        public ReceivingCard(long accountNumber) {
-            this.accountNumber = accountNumber;
-        }
-
-        public long getAccountNumber() { return this.accountNumber; }
-
-        public void setAccountNumber(long accountNumber) { this.accountNumber = accountNumber; }
-    }
-
     private class ReceivingAmount {
         private double value;           //  Cart total cost: XXXXXXXXXX.XX (12,2)
         private String currency;        //  3 characters. Default "USD"
@@ -32,107 +20,121 @@ public class MasterCreditDto {
         public void setCurrency(String currency) { this.currency = currency; }
     }
 
-    private String mcTransactionType;   //  PAYMENT - TransactioTypeEnum.PAYMENT
-    private String mcCardNumber;        //  16 digits
-    private String mcExpirationdate;    //   6 digits: MMYYYY
-    private String mcCustomerName;      //  2-30 characters ([A-Za-z]{2,30})
-    private String customerPhone;       //  0-20 digits and special characters
-    private String mcCVVNumber;         //  3 digits
-    private String transactionDate;     //  8 digits: DDMMYYYY
+    private String MCTransactionType;   //  PAYMENT - TransactioTypeEnum.PAYMENT
+    private long MCCardNumber;          //  16 digits
+    private String MCExpirationdate;       //   6 digits: MMYYYY
+    private String MCCustomerName;      //  2-30 characters ([A-Za-z]{2,30})
+    private String Customerphone;       //  0-20 digits and special characters
+    private String MCCVVNumber;         //  3 digits
+    private String TransactionDate;     //  8 digits: DDMMYYYY
+    //private String MCReceivingCard_AccountNumber;       //  fixed 12 digits. String because can start with "0".
+    private String AccountNumber;       //  fixed 12 digits. String because can start with "0".
 
-    private ReceivingCard receivingCard;
-    private ReceivingAmount receivingAmount;
-
+    private ReceivingAmount MCReceivingAmount;
 
     public MasterCreditDto() { }
 
-    public MasterCreditDto(String mcTransactionType, String mcCardNumber, String mcExpirationdate, String mcCustomerName, String customerPhone, String mcCVVNumber, String transactionDate, long accountNumber, double value, String currency) {
-        this.mcTransactionType = mcTransactionType;
-        this.mcCardNumber = mcCardNumber;
-        this.mcExpirationdate = mcExpirationdate;
-        this.mcCustomerName = mcCustomerName;
-        this.customerPhone = customerPhone;
-        this.mcCVVNumber = mcCVVNumber;
-        this.transactionDate = transactionDate;
+    public MasterCreditDto(String MCTransactionType, long MCCardNumber, String MCExpirationdate, String MCCustomerName, String Customerphone, String mcCVVNumber, String transactionDate, String accountNumber, double value, String currency) {
+        this.MCTransactionType = MCTransactionType;
+        this.MCCardNumber = MCCardNumber;
+        this.MCExpirationdate = MCExpirationdate;
+        this.MCCustomerName = MCCustomerName;
+        this.Customerphone = Customerphone;
+        this.MCCVVNumber = mcCVVNumber;
+        this.TransactionDate = transactionDate;
+        this.AccountNumber = accountNumber;
 
-        this.setReceivingCard(accountNumber);
         this.setReceivingAmount(value, currency);
     }
 
-    public String getMcTransactionType() {
-        return mcTransactionType;
+    public String getMCTransactionType() {
+        return MCTransactionType;
     }
 
-    public void setMcTransactionType(String mcTransactionType) {
-        this.mcTransactionType = mcTransactionType;
+    public void setMCTransactionType(String MCTransactionType) {
+        this.MCTransactionType = MCTransactionType;
     }
 
-    public String getMcCardNumber() {
-        return mcCardNumber;
+    public long getMCCardNumber() {
+        return MCCardNumber;
     }
 
-    public void setMcCardNumber(String mcCardNumber) {
-        this.mcCardNumber = mcCardNumber;
+    public void setMCCardNumber(long MCCardNumber) {
+        this.MCCardNumber = MCCardNumber;
     }
 
-    public String getMcExpirationdate() {
-        return mcExpirationdate;
+    public String getMCExpirationdate() {
+        return MCExpirationdate;
     }
 
-    public void setMcExpirationdate(String mcExpirationdate) {
-        this.mcExpirationdate = mcExpirationdate;
+    public void setMCExpirationdate(String MCExpirationdate) {
+        this.MCExpirationdate = MCExpirationdate;
     }
 
-    public String getMcCustomerName() {
-        return mcCustomerName;
+    public String getMCCustomerName() {
+        return MCCustomerName;
     }
 
-    public void setMcCustomerName(String mcCustomerName) {
-        this.mcCustomerName = mcCustomerName;
+    public void setMCCustomerName(String MCCustomerName) {
+        this.MCCustomerName = MCCustomerName;
     }
 
-    public String getCustomerPhone() {
-        return customerPhone;
+    public String getCustomerphone() {
+        return Customerphone;
     }
 
-    public void setCustomerPhone(String customerPhone) {
-        this.customerPhone = customerPhone;
+    public void setCustomerphone(String customerphone) {
+        this.Customerphone = customerphone;
     }
 
-    public String getMcCVVNumber() {
-        return mcCVVNumber;
+    public String getMCCVVNumber() {
+        return MCCVVNumber;
     }
 
-    public void setMcCVVNumber(String mcCVVNumber) {
-        this.mcCVVNumber = mcCVVNumber;
+    public void setMCCVVNumber(String MCCVVNumber) {
+        this.MCCVVNumber = MCCVVNumber;
     }
 
     public String getTransactionDate() {
-        return transactionDate;
+        return TransactionDate;
     }
 
     public void setTransactionDate(String transactionDate) {
-        this.transactionDate = transactionDate;
+        this.TransactionDate = transactionDate;
     }
 
-    public ReceivingCard getReceivingCard() {
-        return this.receivingCard;
+    public String getAccountNumber() {
+        return this.AccountNumber;
     }
 
-    public void setReceivingCard(long accountNumber) {
-        this.receivingCard = new ReceivingCard(accountNumber);
+    public void setAccountNumber(String accountNumber) {
+        this.AccountNumber = accountNumber;
     }
 
-    public ReceivingAmount getReceivingAmount() {
-        return this.receivingAmount;
+    public ReceivingAmount getMCReceivingAmount() {
+        return this.MCReceivingAmount;
     }
 
-    public void setReceivingAmount(ReceivingAmount receivingAmount) {
-        this.receivingAmount = receivingAmount;
+    public void setMCReceivingAmount(ReceivingAmount MCReceivingAmount) {
+        this.MCReceivingAmount = MCReceivingAmount;
     }
 
     public void setReceivingAmount(double value, String currency) {
-        this.receivingAmount = new ReceivingAmount(value, currency);
+        this.MCReceivingAmount = new ReceivingAmount(value, currency);
     }
 
+    @Override
+    public String toString() {
+        return "MasterCreditDto{" +
+                "MCTransactionType='" + MCTransactionType + '\'' +
+                ", MCCardNumber=" + MCCardNumber +
+                ", MCExpirationdate='" + MCExpirationdate + '\'' +
+                ", MCCustomerName='" + MCCustomerName + '\'' +
+                ", Customerphone='" + Customerphone + '\'' +
+                ", MCCVVNumber='" + MCCVVNumber + '\'' +
+                ", TransactionDate='" + TransactionDate + '\'' +
+                ", MCReceivingCard_AccountNumber='" + AccountNumber + '\'' +
+                ", MCReceivingAmount=" + MCReceivingAmount +
+                '}';
+    }
 }
--- mastercredit/src/main/java/advantage/online/store/services/MasterCreditService.java
+++ mastercredit/src/main/java/advantage/online/store/services/MasterCreditService.java
@@ -4,6 +4,8 @@ import advantage.online.store.dto.MasterCreditDto;
 import advantage.online.store.dto.MasterCreditResponseStatus;
 import advantage.online.store.dto.ResponseEnum;
 import advantage.online.store.dto.TransactionTypeEnum;
+import advantage.util.ArgumentValidationHelper;
+import advantage.util.ValidationHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -35,19 +37,50 @@ public class MasterCreditService {
 
     /**
      * Do <i>MOCK</i> <b>MasterCredit</b> payment. <br/>
-     * Payment is successful unless{@link MasterCreditDto#transactionDate}
+     * Payment is successful unless{@link MasterCreditDto} {@code transactionDate}
      * did not occur yet (is in the future).
      * @param masterCreditDto {@link MasterCreditDto} with payment {@code request} data.
      * @return {@link MasterCreditResponseStatus} <b>MasterCredit</b> server {@code response} information.
      */
     @Transactional
     public MasterCreditResponseStatus doPayment(MasterCreditDto masterCreditDto) {
+
+        ArgumentValidationHelper.validateArgumentIsNotNull(masterCreditDto, "MasterCredit request data");
+        ArgumentValidationHelper.validateLongArgumentIsPositive(masterCreditDto.getMCCardNumber(), "MasterCredit card number");
+
+        ArgumentValidationHelper.validateStringArgumentIsNotNullAndNotBlank(masterCreditDto.getMCTransactionType(), "MasterCredit transaction type");
+        ArgumentValidationHelper.validateStringArgumentIsNotNullAndNotBlank(masterCreditDto.getMCExpirationdate(), "MasterCredit expiration date");
+        ArgumentValidationHelper.validateStringArgumentIsNotNullAndNotBlank(masterCreditDto.getMCCustomerName(), "MasterCredit customer name");
+        ArgumentValidationHelper.validateStringArgumentIsNotNullAndNotBlank(masterCreditDto.getCustomerphone(), "MasterCredit customer phone");
+        ArgumentValidationHelper.validateStringArgumentIsNotNullAndNotBlank(masterCreditDto.getMCCVVNumber(), "MasterCredit CVV number");
+        ArgumentValidationHelper.validateStringArgumentIsNotNullAndNotBlank(masterCreditDto.getTransactionDate(), "MasterCredit transaction date");
+        ArgumentValidationHelper.validateStringArgumentIsNotNullAndNotBlank(masterCreditDto.getAccountNumber(), "MasterCredit receiving card account number");
+
+        boolean isValid = ValidationHelper.isValidDate(masterCreditDto.getTransactionDate().substring(1, 2) + "." +
+                                                        masterCreditDto.getTransactionDate().substring(4, 5) + "." +
+                                                        masterCreditDto.getTransactionDate().substring(6, 8));
+
+        isValid = isValid && ValidationHelper.isValidMasterCreditCVVNumber(masterCreditDto.getMCCVVNumber());
+
+        isValid = isValid && ValidationHelper.isValidDate("01." +
+                                    masterCreditDto.getMCExpirationdate().substring(1, 2) + "." +
+                                    masterCreditDto.getMCExpirationdate().substring(3, 6));
+
+        isValid = isValid && ValidationHelper.isValidMasterCreditCVVNumber(masterCreditDto.getMCCVVNumber());
+        isValid = isValid && ValidationHelper.isValidFullName(masterCreditDto.getMCCustomerName());
+
         MasterCreditResponseStatus responseStatus = new MasterCreditResponseStatus();
 
-        responseStatus.setMCTransactionType(TransactionTypeEnum.REFUND.getStringCode());
-        responseStatus.setMCResponse(ResponseEnum.APPROVED.getStringCode());
+        responseStatus.setMCTransactionType(TransactionTypeEnum.PAYMENT.getStringCode());
         responseStatus.setMCRefNumber(this.referenceNumberNextValue());
-        responseStatus.setTransactionDate(new SimpleDateFormat("ddMMyyyy").format(new Date()));
+        //responseStatus.setTransactionDate(new SimpleDateFormat("ddMMyyyy").format(new Date()));
+        responseStatus.setTransactionDate(masterCreditDto.getTransactionDate());
+
+        if (isValid) {
+            responseStatus.setMCResponse(ResponseEnum.APPROVED.getStringCode());
+        } else {
+            responseStatus.setMCResponse(ResponseEnum.REJECTED.getStringCode());
+        }
 
         return responseStatus;
     }
