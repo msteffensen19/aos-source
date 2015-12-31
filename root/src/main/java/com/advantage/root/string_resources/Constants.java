3b08ecb9380c574fc5e60d5c4d4d9fbdeeb629b4
Evgeney Fiskin <evgeney.fiskin@hpe.com>
2015-12-31 17:02:43 +0200

    user story #1442: Move all common classes to "root" project Constants changes

--- root/src/main/java/com/advantage/root/string_resources/Constants.java
+++ root/src/main/java/com/advantage/root/string_resources/Constants.java
@@ -5,6 +5,7 @@ public class Constants {
 
     public static final String URI_API = "/api";
 
+    //
     public static final String PROPERTIES_INTERNAL = "classpath:/services.properties";
     public static final String PROPERTIES_EXTERNAL = "classpath:/services.properties";
 
