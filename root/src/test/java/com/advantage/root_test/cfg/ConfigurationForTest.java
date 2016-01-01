9aadd0cb4f83e0b9a320576c4d0982813f237127
Evgeney Fiskin <evgeney.fiskin@hpe.com>
2016-01-01 19:39:44 +0200

    Create test for property file

---
+++ root/src/test/java/com/advantage/root_test/cfg/ConfigurationForTest.java
@@ -0,0 +1,12 @@
+package com.advantage.root_test.cfg;
+
+/**
+ * Created by Evgeney Fiskin on 01-01-2016.
+ */
+public class ConfigurationForTest {
+    public static final String[] conponentsForScan = {"com.advantage.order.store.services",
+            "com.advantage.order.store.dao",
+            "com.advantage.order.store.user.dao",
+            "com.advantage.order.store.user.model",
+            "com.advantage.order.store.init"};
+}
