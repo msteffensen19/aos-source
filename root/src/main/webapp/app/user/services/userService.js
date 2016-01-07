65275cc7966de93910ace42739623dd961efc22d
correnti <efi.correnti@hpe.com>
2016-01-07 21:51:03 +0200

    soap -

--- root/src/main/webapp/app/user/services/userService.js
+++ root/src/main/webapp/app/user/services/userService.js
@@ -32,19 +32,7 @@ define(['./module'], function (services) {
             //response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
             //response.setHeader("Access-Control-Max-Age", "3600");
             //response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Key, soapaction");
-
-                $soap.post(
-                    'http://www.advantageonlineshopping.com/accountservice',
-                    'GetAccountByIdRequest',
-                    { accountId: 12 }
-                ).then(function(response){
-                    console.log("angular soap SUCCESS")
-                    console.log(response);
-                },
-                function(response){
-                    console.log("angular soap FAILD")
-                    console.log(response);
-                });;
+            //
 
             function login(user) {
 
