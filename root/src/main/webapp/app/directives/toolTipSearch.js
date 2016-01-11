eb28a506a9c2bda75006540bec323b38cd4c32fb
Correnti <efi.correnti@hpe.com>
2016-01-11 22:59:39 +0200

    user story #1350: Screen "view all" after searching products

--- root/src/main/webapp/app/directives/toolTipSearch.js
+++ root/src/main/webapp/app/directives/toolTipSearch.js
@@ -4,12 +4,80 @@
 
 define(['./module'], function (directives) {
     'use strict';
-    directives.directive('toolTipSearch', ['$templateCache',
-        function ($templateCache) {
+    directives.directive('toolTipSearch', ['$templateCache', 'productService',
+        function ($templateCache, productService) {
         return {
             restrict: 'E',
             replace: true,
             template: $templateCache.get('app/partials/toolTipSearch.html'),
+            link: function (s){
+
+                s.categoryFilter = null;
+                s.checkEnterKey = function(event)
+                {
+                    if(event.which === 13) {
+                        console.log(event)
+                        console.log(event.which)
+                    }
+                }
+
+                var lastRequest = '';
+                s.runAutocomplete = function(){
+                    lastRequest = s.autoCompleteValue;
+                    if(lastRequest == '') {
+                        s.autoCompleteResult = {};
+                        return;
+                    }
+                    productService.getProductsBySearch(lastRequest, 10).then(function(result){
+                        s.autoCompleteResult = result;
+                        console.log(event)
+                    });
+                }
+
+
+                s.openSearchProducts = function(){
+                    $("nav ul li a.navLinks").stop().animate({ opacity : 0 }, 400);
+                    setTimeout(function(_this){
+                        $("#searchSection").fadeIn(1000);
+                        $("#autoComplete").focus();
+                        $("#searchSection > div:first-child > div").addClass("searchSectionEnabled");
+                        $("#searchSection > div > div > span > img").delay(500).fadeIn(500); // img close
+                        $('#openSearch').stop().animate({ opacity : 0 }, 300)
+                    }, 400);
+                }
+
+                s.closeSearchSection = function(){
+                    $('#openSearch').stop().animate({ opacity : 1 }, 300)
+                    $("#searchSection > div > div > span > img").fadeOut(200); // img close
+                    setTimeout(function(){
+                        $("#searchSection").fadeOut(500);
+                        $("#searchSection > div:first-child > div").removeClass("searchSectionEnabled");
+                        if($location.$$path == '/')
+                        {
+                            $("nav ul li a.navLinks").stop().animate({ opacity : 1 }, 400);
+                        }
+                        s.autoCompleteValue = lastRequest = '';
+                        s.autoCompleteResult = {};
+                        $("#autoComplete").focusout();
+                    }, 200)
+                }
+
+                $('#product_search_img').click(function (e) {
+                    $('#product_search').css("display", "inline-block");
+                    $('#product_search').animate({ "width": $('#product_search').width() > 0 ? 0 : "150px" },
+                        500, function(){
+                            if($('#product_search').width() == 0 ){
+                                $(this).css("display", "none");
+                            }
+                        } );
+                });
+
+                s.searchByCategoryId = function(id){
+                    console.log(id);
+                    s.categoryFilter = id;
+                }
+
+            }
         };
     }]);
 });
--- root/src/main/webapp/app/filters/filterFullArrayforAutoComplate.js
+++ root/src/main/webapp/app/filters/filterFullArrayforAutoComplate.js
@@ -5,21 +5,31 @@
 define(['./module'], function (controllers) {
     'use strict';
     controllers.filter('filterFullArrayforAutoComplate', function(){
-        return function(_a, autoCompleteResult) {
-            if(autoCompleteResult[0] == undefined)
-            {
+        return function(_a, autoCompleteResult, categoryFilter) {
+
+            if(autoCompleteResult[0] == undefined){
                 return _a;
             }
-
             var products = [];
-            angular.forEach(autoCompleteResult.products, function(product){
-
-            })
-            for(var i = 0; i < 6 && i < autoCompleteResult[0].products.length; i++)
-            {
-                products.push(autoCompleteResult[0].products[i]);
+            var max = 0;
+            for (var i = 0; i < autoCompleteResult.length; i++){
+                max = autoCompleteResult[i].products.length > max ? autoCompleteResult[i].products.length : max;
             }
-            console.log(products)
+            for (var i = 0; i < max; i++){
+                for (var j = 0; j < autoCompleteResult.length; j++){
+
+                    if(categoryFilter == autoCompleteResult[j].categoryId || categoryFilter == null)
+                    {
+                        var prd = autoCompleteResult[j].products[i]
+                        if(prd){
+                            products.push(prd);
+                            if(products.length == 6){
+                                return products;
+                            }
+                        }
+                    }
+                }
+            }
             return products;
         };
     });
