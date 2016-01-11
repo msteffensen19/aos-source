571166227f0fe4070e177dddfd78f7fce88dcc13
Correnti <efi.correnti@hpe.com>
2016-01-11 10:36:16 +0200

    user story #1354: AccountType to 20 & soap

--- root/src/main/webapp/app/user/controllers/registerCtrl.js
+++ root/src/main/webapp/app/user/controllers/registerCtrl.js
@@ -35,14 +35,14 @@ define(['./module'], function (controllers) {
                 //check validateModel(model)
 
                 registerService.register(s.model).then(function (response) {
-                    s.registerAnswer.message = response.reason,
-                        s.registerAnswer.class = response.success ? 'valid' : 'invalid';
+                    s.registerAnswer.message = response.REASON,
+                        s.registerAnswer.class = response.SUCCESS == 'true' ? 'valid' : 'invalid';
                     $timeout(function (success) {
                         s.registerAnswer = {message: '', class: 'invalid'}
                         if (success) {
                             window.history.back();
                         }
-                    }, 4000, response.success)
+                    }, 4000, response.SUCCESS)
                 });
             }
 
@@ -55,25 +55,6 @@ define(['./module'], function (controllers) {
             });
 
             $("nav .navLinks").css("display", "none");
-
-            /*
-            $scope.inputFocus = function (id) {
-                console.log($('#' + id));
-                $('#' + id).siblings().not("img").not('.validationInfo').not('.must').animate(
-                    {'top': '-10px'}, 800, $.bez([0.62, -0.14, 0.35, 1.34]));
-                $('#' + id).siblings(".validationInfo").fadeIn();
-
-            }
-
-            $scope.inputBlur = function (id, validation) {
-
-                if ($('#' + id).val() == '') {
-                    $('#' + id).siblings().not("img").not('.validationInfo').animate({'top': '11px'}, 800, $.bez([0.62, -0.14, 0.35, 1.34]));
-                }
-                $('#' + id).siblings(".validationInfo").delay(200).fadeOut();
-
-            }
-            */
 
         }]);
 
