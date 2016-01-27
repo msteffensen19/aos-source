5fa96b542b8487348f548a1a45f19478169730a0
Correnti <efi.correnti@hpe.com>
2016-01-27 09:05:44 +0200

    defect #35: Remarks from UI session 26/01/2015

--- root/src/main/webapp/app/directives/toolTipSearch.js
+++ root/src/main/webapp/app/directives/toolTipSearch.js
@@ -36,39 +36,60 @@ define(['./module'], function (directives) {
 
 
                 s.openSearchProducts = function(){
-                    $("nav ul li a.navLinks").stop().animate({ opacity : 0 }, 400, function(){
-                        $(this).hide();
+
+                    var navsLinks = $("nav ul li a.navLinks");
+                    navsLinks.each(function(index){
+                        setTimeout(function(_this){
+                            $(_this).stop().animate({ opacity : 0 }, 100);
+                        }, 100 * index, this);
                     });
+                    setTimeout(function(navsLinks){
+                        navsLinks.hide();
+                    }, 200 * navsLinks.length, navsLinks);
+
                     setTimeout(function(){
-                        $('#openSearch').hide();
-                        $("#searchSection").css('display', 'block');//.fadeIn(200);
-                        $("#autoComplete").focus();
-                        $("div#search").animate({
-                            left: 0,
-                            opacity: 1
-                        }, 700);
-                        //$("#searchSection div#search").addClass("searchSectionEnabled");
-                        $("#searchSection > div > div > span > .img").delay(700).fadeIn(500); // img close
-                    }, 400);
+
+                        var width = $(document).width() < 1200 ? 400 : 700;
+                        $('#searchSection .autoCompleteCover').animate({
+                            width : width + "px",
+                        }, 500, function(){
+
+                            var docWidth = $(document).width();
+
+                            alert($("#searchSection").width());
+                            alert($(window).width());
+                            alert($(".logo").width());
+                        });
+
+                        //$('#openSearch').hide();
+                        //$("#searchSection").css('display', 'block');//.fadeIn(200);
+                        //$("#autoComplete").focus();
+                        //$("div#search").animate({
+                        //    left: 0,
+                        //    opacity: 1
+                        //}, 700);
+                        ////$("#searchSection div#search").addClass("searchSectionEnabled");
+                        //$("#searchSection > div > div > span > .img").delay(700).fadeIn(500); // img close
+                    }, 400, navsLinks);
                 }
 
                 s.closeSearchSection = function(){
-                    $("#searchSection").fadeOut(200);
-                    $("div#search").animate({
-                        left: '920px',
-                        opacity: 0
-                    }, 700, function(){
-                        //$("#searchSection div#search").addClass("searchSectionEnabled");
-                        $('#openSearch').show();
-                        $("#searchSection > div > div > span > .img").delay(500).fadeOut(500);
-                        setTimeout(function(){
-                            $("nav ul li a.navLinks").stop().animate({ opacity : 1 }, 400, function(){
-                                $(this).show();
-                            });
-                        }, 400);
-                        s.autoCompleteValue = lastRequest = '';
-                        s.autoCompleteResult = {};
-                    });
+                    //$("#searchSection").fadeOut(200);
+                    //$("div#search").animate({
+                    //    left: '920px',
+                    //    opacity: 0
+                    //}, 700, function(){
+                    //    //$("#searchSection div#search").addClass("searchSectionEnabled");
+                    //    $('#openSearch').show();
+                    //    $("#searchSection > div > div > span > .img").delay(500).fadeOut(500);
+                    //    setTimeout(function(){
+                    //        $("nav ul li a.navLinks").stop().animate({ opacity : 1 }, 400, function(){
+                    //            $(this).show();
+                    //        });
+                    //    }, 400);
+                    //    s.autoCompleteValue = lastRequest = '';
+                    //    s.autoCompleteResult = {};
+                    //});
 
                 }
 
