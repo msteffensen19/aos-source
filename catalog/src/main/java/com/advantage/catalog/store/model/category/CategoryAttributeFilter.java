227e8f91ac5793625c748cf3332ef40993b3e617
moti ostrovski <moti.ostrovski@hpe.com>
2016-01-31 16:46:35 +0200

    user story #1523: As a user in category I want to filter only by attributes which are filterable

---
+++ catalog/src/main/java/com/advantage/catalog/store/model/category/CategoryAttributeFilter.java
@@ -0,0 +1,67 @@
+package com.advantage.catalog.store.model.category;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+
+/**
+ * Created by ostrovsm on 31/01/2016.
+ */
+public class CategoryAttributeFilter implements Serializable {
+
+    private Long categoryId;
+    private Long attributeId;
+    private boolean inFilter;
+
+    public CategoryAttributeFilter(){}
+
+    public CategoryAttributeFilter(long categoryId, long attributeId){
+        this.categoryId=categoryId;
+        this.attributeId=attributeId;
+        this.inFilter=true; //default value is show in filter
+    }
+
+    public CategoryAttributeFilter(long categoryId, long attributeId,boolean inFilter ){
+        this.categoryId=categoryId;
+        this.attributeId=attributeId;
+        this.inFilter=inFilter;
+    }
+
+    public Long getCategoryId() { return categoryId;  }
+
+    public void setCategoryId(Long categoryId) {this.categoryId = categoryId; }
+
+    public Long getAttributeId() { return attributeId; }
+
+    public void setAttributeId(Long attributeId) { this.attributeId = attributeId; }
+
+    public boolean isInFilter() { return inFilter;}
+
+    public void setInFilter(boolean inFilter) {this.inFilter = inFilter; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CategoryAttributeFilter that = (CategoryAttributeFilter) o;
+
+        if (!categoryId.equals(that.categoryId)) return false;
+        return attributeId.equals(that.attributeId);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = categoryId.hashCode();
+        result = 31 * result + attributeId.hashCode();
+        return result;
+    }
+}
+
+
+
+
+
+
+
