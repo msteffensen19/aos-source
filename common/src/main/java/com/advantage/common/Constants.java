b36e37928a27b8b7d385e96b481feddd05b81e22
regevb@hpe.com <regevb@hpe.com>
2016-02-28 10:30:48 +0200

    defect #90: Support searching parameters of more than one tool

--- common/src/main/java/com/advantage/common/Constants.java
+++ common/src/main/java/com/advantage/common/Constants.java
@@ -13,7 +13,6 @@ public class Constants {
     public static final String FILE_PROPERTIES_GLOBAL = "classpath:/global.properties";
     public static final String FILE_PROPERTIES_DEMO_APP = "classpath:/DemoApp.properties";
     public static final String FILE_PROPERTIES_APP = "classpath:/app.properties";
-    public static final String FILE_CONFIG_DEMO_APP = "classpath:/DemoApp.config";
     public static final String FILE_DEMO_APP_CONFIG_XML = "classpath:/DemoAppConfig.xml";
 
 
