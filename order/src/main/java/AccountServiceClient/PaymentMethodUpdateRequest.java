e5765e5e268d2178597a986ba676be931325b0e1
regevb@hpe.com <regevb@hpe.com>
2016-03-07 19:37:57 +0200

    defect #100: Recreate Accountservice WSDL in Order service

---
+++ order/src/main/java/AccountServiceClient/PaymentMethodUpdateRequest.java
@@ -0,0 +1,73 @@
+
+package AccountServiceClient;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="accountId" type="{http://www.w3.org/2001/XMLSchema}long"/>
+ *         &lt;element name="paymentMethod" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "accountId",
+    "paymentMethod"
+})
+@XmlRootElement(name = "PaymentMethodUpdateRequest")
+public class PaymentMethodUpdateRequest {
+
+    protected long accountId;
+    protected int paymentMethod;
+
+    /**
+     * Gets the value of the accountId property.
+     * 
+     */
+    public long getAccountId() {
+        return accountId;
+    }
+
+    /**
+     * Sets the value of the accountId property.
+     * 
+     */
+    public void setAccountId(long value) {
+        this.accountId = value;
+    }
+
+    /**
+     * Gets the value of the paymentMethod property.
+     * 
+     */
+    public int getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    /**
+     * Sets the value of the paymentMethod property.
+     * 
+     */
+    public void setPaymentMethod(int value) {
+        this.paymentMethod = value;
+    }
+
+}
---
+++ order/src/main/java/AccountServiceClient/UpdateDemoAppConfigParameterResponse.java
@@ -0,0 +1,169 @@
+
+package AccountServiceClient;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="StatusMessage" form="qualified">
+ *           &lt;complexType>
+ *             &lt;complexContent>
+ *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *                 &lt;sequence>
+ *                   &lt;element name="success" type="{http://www.w3.org/2001/XMLSchema}boolean" form="qualified"/>
+ *                   &lt;element name="reason" type="{http://www.w3.org/2001/XMLSchema}string" form="qualified"/>
+ *                   &lt;element name="code" type="{http://www.w3.org/2001/XMLSchema}long" form="qualified"/>
+ *                 &lt;/sequence>
+ *               &lt;/restriction>
+ *             &lt;/complexContent>
+ *           &lt;/complexType>
+ *         &lt;/element>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "statusMessage"
+})
+@XmlRootElement(name = "UpdateDemoAppConfigParameterResponse")
+public class UpdateDemoAppConfigParameterResponse {
+
+    @XmlElement(name = "StatusMessage", required = true)
+    protected UpdateDemoAppConfigParameterResponse.StatusMessage statusMessage;
+
+    /**
+     * Gets the value of the statusMessage property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link UpdateDemoAppConfigParameterResponse.StatusMessage }
+     *     
+     */
+    public UpdateDemoAppConfigParameterResponse.StatusMessage getStatusMessage() {
+        return statusMessage;
+    }
+
+    /**
+     * Sets the value of the statusMessage property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link UpdateDemoAppConfigParameterResponse.StatusMessage }
+     *     
+     */
+    public void setStatusMessage(UpdateDemoAppConfigParameterResponse.StatusMessage value) {
+        this.statusMessage = value;
+    }
+
+
+    /**
+     * <p>Java class for anonymous complex type.
+     * 
+     * <p>The following schema fragment specifies the expected content contained within this class.
+     * 
+     * <pre>
+     * &lt;complexType>
+     *   &lt;complexContent>
+     *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+     *       &lt;sequence>
+     *         &lt;element name="success" type="{http://www.w3.org/2001/XMLSchema}boolean" form="qualified"/>
+     *         &lt;element name="reason" type="{http://www.w3.org/2001/XMLSchema}string" form="qualified"/>
+     *         &lt;element name="code" type="{http://www.w3.org/2001/XMLSchema}long" form="qualified"/>
+     *       &lt;/sequence>
+     *     &lt;/restriction>
+     *   &lt;/complexContent>
+     * &lt;/complexType>
+     * </pre>
+     * 
+     * 
+     */
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "", propOrder = {
+        "success",
+        "reason",
+        "code"
+    })
+    public static class StatusMessage {
+
+        protected boolean success;
+        @XmlElement(required = true)
+        protected String reason;
+        protected long code;
+
+        /**
+         * Gets the value of the success property.
+         * 
+         */
+        public boolean isSuccess() {
+            return success;
+        }
+
+        /**
+         * Sets the value of the success property.
+         * 
+         */
+        public void setSuccess(boolean value) {
+            this.success = value;
+        }
+
+        /**
+         * Gets the value of the reason property.
+         * 
+         * @return
+         *     possible object is
+         *     {@link String }
+         *     
+         */
+        public String getReason() {
+            return reason;
+        }
+
+        /**
+         * Sets the value of the reason property.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link String }
+         *     
+         */
+        public void setReason(String value) {
+            this.reason = value;
+        }
+
+        /**
+         * Gets the value of the code property.
+         * 
+         */
+        public long getCode() {
+            return code;
+        }
+
+        /**
+         * Sets the value of the code property.
+         * 
+         */
+        public void setCode(long value) {
+            this.code = value;
+        }
+
+    }
+
+}
