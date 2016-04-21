2528e15ea97afb6cadc99c432b0de4fd9943ff39
Asaph Efrati <asaph.efrati@hpe.com>
2016-04-21 13:05:54 +0300

    scripts

--- scripts/promote-artifact.sh
+++ scripts/promote-artifact.sh
@@ -1,2 +1,3 @@
 echo "TEST"
+PATH=/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/usr/java/jdk1.8.0_74/jre:/usr/java/jdk1.8.0_74/jre:/opt/apache-maven-3.3.9/bin/:/opt/node-v4.4.0-linux-x64/bin/
 which mvn
