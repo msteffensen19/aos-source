2e5091fd7c6b13e197699e5423a948080a492adb
regevb@hpe.com <regevb@hpe.com>
2016-05-09 15:12:33 +0300

    defect #136: User configuration parameters - needs some fixing

--- safepay/src/main/java/com/advantage/safepay/payment/services/SafePayService.java
+++ safepay/src/main/java/com/advantage/safepay/payment/services/SafePayService.java
@@ -93,11 +93,18 @@ public class SafePayService {
         }
 
         /*  User name*/
-        if (safePayDto.getPassword() == null | safePayDto.getPassword().length() < 1 || safePayDto.getPassword().length() > 20) {
+        if (safePayDto.getPassword() == null | safePayDto.getPassword().length() < 1 ) {
             responseStatus.setResponseCode(ResponseEnum.ERROR.getStringCode());
             responseStatus.setResponseReason("Wrong field value. Field \'SPPassword\' value=" + safePayDto.getPassword());
             responseStatus.setReferenceNumber(0);
             isValid = false;
+        }
+        else if (safePayDto.getPassword().length() > 20) {
+            //  SafePay Password ENCRYPTED - Get first 20 characters
+            safePayDto.setPassword(safePayDto.getPassword().substring(0, 19));
+        }
+        else {
+            //  SafePay Password size is 1-20 characters = Typed Password as is
         }
 
 //        if (isValid) {
