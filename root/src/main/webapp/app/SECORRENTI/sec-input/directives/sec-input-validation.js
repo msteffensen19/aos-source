a903432d33e6eb80e2affdac28710c05f0b0a996
Correnti <efi.correnti@hpe.com>
2016-05-22 17:40:59 +0300

    Products 22/05/2016

--- root/src/main/webapp/app/SECORRENTI/sec-input/directives/sec-input-validation.js
+++ root/src/main/webapp/app/SECORRENTI/sec-input/directives/sec-input-validation.js
@@ -1,6 +1,7 @@
-/**
+/*
+/!**
  * Created by correnti on 19/05/2016.
- */
+ *!/
 
 
 define(['./../../../directives/module'], function (directives) {
@@ -269,3 +270,4 @@ define(['./../../../directives/module'], function (directives) {
 });
 
 
+*/
