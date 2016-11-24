70e132e7a42a19eba67365171dd755059d421249
regevb@hpe.com <regevb@hpe.com>
2016-11-24 14:49:23 +0200

    Defect #330 - Add shipexFreeNoOfProducts to the .NET admin config UI (Back End)

--- shipex/src/main/java/ShippingExpress/model/DefaultShippingExpressRepository.java
+++ shipex/src/main/java/ShippingExpress/model/DefaultShippingExpressRepository.java
@@ -1,17 +1,23 @@
 package ShippingExpress.model;
 
+import com.advantage.root.util.RestApiHelper;
+import com.advantage.root.util.StringHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Repository;
+//import com.advantage.root.util.RestApiHelper;
 
-
+/**
+ * @author Binyamin Regev Created on 26/03/2016.
+ */
 @Qualifier("shipexRepository")
 @Repository
 public class DefaultShippingExpressRepository implements ShippingExpressRepository {
     public static final String SHIPEX_DEFAULT_COST = "shipex.country.defaultCost";
     public static final String SHIPEX_FREE_SHIPPING_OPTION = "shipex.free.numberOfProducts";
     public static final String SHIPEX_CURRENCY_DEFAULT = "shipex.currency.default";
+    public static final String SHIPEX_QUANTITY_ENTITLING_FREE_SHIPPING = "ShipEx_Free_Number_Of_Products";
     private Environment env;
 
     @Autowired
@@ -32,9 +38,22 @@ public class DefaultShippingExpressRepository implements ShippingExpressReposito
         return Double.parseDouble(env.getProperty(SHIPEX_DEFAULT_COST));
     }
 
+    /**
+     * Get quantity of products that entitle <i>Free Shipping</i> based on
+     * &lt;ShipEx_Free_Number_Of_Products&gt; configuration parameter. <br/>
+     * The parameter must contains a valid int, or the server will return 0. <br/>
+     * Default value for the parameter is 1.
+     * @return {@code int} value of the configuration parameter or 0 if the
+     * value is invalid, meaning negative or not an int.
+     */
     @Override
     public int getFreeShippingOption() {
-        return Integer.parseInt(env.getProperty(SHIPEX_FREE_SHIPPING_OPTION));
+        //return Integer.parseInt(env.getProperty(SHIPEX_FREE_SHIPPING_OPTION));
+        String stringValue = RestApiHelper.getDemoAppConfigParameterValue(SHIPEX_QUANTITY_ENTITLING_FREE_SHIPPING);
+        if ((stringValue != null) && (!stringValue.isEmpty()) && StringHelper.isInt(stringValue)) {
+            return Integer.parseInt(stringValue) >= 0 ? Integer.parseInt(stringValue) : 0;
+        }
+        return 0;
     }
 
     @Override
