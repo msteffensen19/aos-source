89d388cf6b3e1098ef02918044aef23ae6689f9d
Kostiantyn Bukhantsov <kostiantyn.bukhantsov@hpe.com>
2017-04-05 09:42:44 +0300

    defect #28012: Web only: invalid user name or password message disappears too quickly Updated timeout from 0 to 10 seconds for showing message

--- root/src/main/webapp/app/user/directives/loginModal.js
+++ root/src/main/webapp/app/user/directives/loginModal.js
@@ -27,7 +27,7 @@ define(['./module'], function (directives) {
                             $scope.message._class = "";
                             $rootScope.rsMessage.text = "";
                             $rootScope.rsMessage._class = "";
-                        }, 0);
+                        }, 10);
                         $timeout(function () {
                             $scope.message = { text: reason, _class: "invalid" }
                             $rootScope.rsMessage = { text: reason, _class: "invalid" }
