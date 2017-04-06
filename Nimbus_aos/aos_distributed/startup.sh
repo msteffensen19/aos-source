352a2005efaf4ab92dc612103060f24cdcd9b480
Michal Edery <michal.edery@hpe.com>
2017-04-06 10:25:10 +0300

    removed the --pull from startup.sh script

--- Nimbus_aos/aos_distributed/startup.sh
+++ Nimbus_aos/aos_distributed/startup.sh
@@ -2,5 +2,5 @@
 ip=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
 command1="sed -i 's/HOST_IP_CALCULATED/$ip/g' .env"
 eval $command1
-docker-compose up --pull -d
+docker-compose up -d
 
