3f1f6c7c947828407609e5ffbae484146330cfb6
Michal Edery <michal.edery@hpe.com>
2017-04-17 14:21:32 +0300

    edited Nimbus_aos/aos_distributed

--- Nimbus_aos/aos_distributed/startup.sh
+++ Nimbus_aos/aos_distributed/startup.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-ip=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
-command1="sed -i 's/HOST_IP_CALCULATED/$ip/g' .env"
-eval $command1
-docker login -u=advantageonlineshoppingapp -p=W3lcome1
-docker-compose up -d
+. .env
 
+sed -i "s/POSTGRES_PORT/${POSTGRES_PORT}/g" docker-compose.yml
+sed -i "s/MAIN_PORT/${MAIN_PORT}/g" docker-compose.yml
+sed -i "s/ACCOUNT_PORT/${ACCOUNT_PORT}/g" docker-compose.yml
+
+# change host name
+ docker node ls | grep -v Leader | grep -v HOSTNAME | awk '{print $2}' | while read line; do command="sed -i '0,/HOST_NAME/{s/HOST_NAME/$line/}' docker-compose.yml"; echo $command; eval $command; done
+# change public ip
+ docker node ls | grep -v Leader | grep -v HOSTNAME | awk '{print $2}' | xargs docker inspect $1 | grep "Addr" | awk '{ gsub("\"",""); print $2}' | while read line; do command="sed -i '0,/PUBLIC_IP/{s/PUBLIC_IP/$line/}' .env"; echo $command; eval $command; done
+#ip of the host
+public_ip=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
+command4="sed -i 's/PUBLIC_IP/${public_ip}/g' .env"
+eval $command4
+#host name of the host
+host_name=`docker node ls | grep -w Leader | awk '{print $3}'`
+command5="sed -i 's/HOST_NAME/${host_name}/g' docker-compose.yml"
+eval $command5
+
+docker login -u=advantageonlineshoppingapp -p=W3lcome1
+docker stack deploy --with-registry-auth -c docker-compose.yml STACK
