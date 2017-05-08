13386be2c0295e5fd150b1ee51518e11a8047a8c
Michal Edery <michal.edery@hpe.com>
2017-05-08 18:11:13 +0300

    updated aos_pipline/aos_distributed to be with docker registry

--- Nimbus_aos/aos_pipline/aos_distributed/startup.sh
+++ Nimbus_aos/aos_pipline/aos_distributed/startup.sh
@@ -32,6 +32,7 @@ eval $command2
 sed -i "s/POSTGRES_PORT/${POSTGRES_PORT}/g" docker-compose.yml
 sed -i "s/MAIN_PORT/${MAIN_PORT}/g" docker-compose.yml
 sed -i "s/ACCOUNT_PORT/${ACCOUNT_PORT}/g" docker-compose.yml
+sed -i "s/REGISTRY_PORT/${REGISTRY_PORT}/g" docker-compose.yml
 sed -i "s/JENKINS_PORT/${JENKINS_PORT}/g" docker-compose.yml
 sed -i "s/TAG/${TAG}/g" docker-compose.yml
 
@@ -39,22 +40,16 @@ sed -i "s/TAG/${TAG}/g" docker-compose.yml
  docker node ls | grep -v Leader | grep -v HOSTNAME | awk '{print $2}' | while read line; do command="sed -i '0,/HOST_NAME/{s/HOST_NAME/$line/}' docker-compose.yml"; echo $command; eval $command; done
 # change public ip
  docker node ls | grep -v Leader | grep -v HOSTNAME | awk '{print $2}' | xargs docker inspect $1 | grep "Addr" | awk '{ gsub("\"",""); print $2}' | while read line; do command="sed -i '0,/PUBLIC_IP_CALCULATED/{s/PUBLIC_IP_CALCULATED/$line/}' .env"; echo $command; eval $command; done
-#ip of the host
-if [ "${PUBLIC_IP}" = "LOCAL" ]; then
- interface=`route | grep -w "default" | awk '{print $8}'`
- ip=`ifconfig | grep -A1 ${interface} | awk -F":" '/inet addr/ {print $2}' | awk '{print $1}'`
-elif [ "${PUBLIC_IP}" = "AMAZON" ]; then
- ip=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
-else
- ip=${PUBLIC_IP}
-fi
-#public_ip=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
-command3="sed -i 's/PUBLIC_IP_CALCULATED/${ip}/g' .env"
-eval $command3
-#host name of the host
-host_name=`docker node ls | grep -w Leader | awk '{print $3}'`
-command4="sed -i 's/HOST_NAME/${host_name}/g' docker-compose.yml"
-eval $command4
+
+# host name
+ command3="sed -i 's/HOST_NAME/$(docker node ls | grep -w Leader | awk '{print $3}')/' docker-compose.yml"
+ eval $command3
+# ip of the host
+ command4="sed -i 's/PUBLIC_IP_CALCULATED/$(docker node ls | grep -w Leader | docker inspect $(awk '{print $3}') | grep -m2 "Addr" | tail -n1 | awk '{ gsub("\"",""); print $2}' | awk -F":" '{print $1}')/' .env"
+ eval $command4
 
 docker login -u=advantageonlineshoppingapp -p=W3lcome1
 docker stack deploy --with-registry-auth -c docker-compose.yml STACK
+
+command5="sed -i 's/performancetesting\/aos-accountservice.*/${REGISTRY_IP}:5000\/aos-accountservice/g' docker-compose.yml"
+eval $command5
