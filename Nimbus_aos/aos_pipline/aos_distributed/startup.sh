c106f3508aa68dbb1d8f6c7d208cc447158ffb7a
Michal Edery <michal.edery@hpe.com>
2017-05-09 16:02:06 +0300

    added to startup.sh in aos_pipline/aos_distributed the creation of post-commit & daemon.json

--- Nimbus_aos/aos_pipline/aos_distributed/startup.sh
+++ Nimbus_aos/aos_pipline/aos_distributed/startup.sh
@@ -7,7 +7,7 @@ one_level_up_workspace="${workspace%/*}"
 two_levels_up_workspace="${one_level_up_workspace%/*}"
 three_levels_up_workspace="${two_levels_up_workspace%/*}"
 three_levels_up_workspace=$(echo "$three_levels_up_workspace" | sed 's/\//\\\//g')
-command1="sed -i 's/WORKSPACE/${newstring}\/accountservice/g' docker-compose.yml"
+command1="sed -i 's/WORKSPACE/${three_levels_up_workspace}\/accountservice/g' docker-compose.yml"
 eval $command1
 
 workspace=$(echo "$workspace" | sed 's/\//\\\//g')
@@ -32,6 +32,13 @@ sed -i "s/TAG/${TAG}/g" docker-compose.yml
 # ip of the host
  command4="sed -i 's/PUBLIC_IP_CALCULATED/$(docker node ls | grep -w Leader | docker inspect $(awk '{print $3}') | grep -m2 "Addr" | tail -n1 | awk '{ gsub("\"",""); print $2}' | awk -F":" '{print $1}')/' .env"
  eval $command4
+
+. .env
+ssh-keyscan ${ACCOUNT_IP} >> /root/.ssh/known_hosts
+echo "{ \"insecure-registries\":[\"${REGISTRY_IP}:${REGISTRY_PORT}\"] }" | ssh root@"${ACCOUNT_IP}" "cat > /etc/docker/daemon.json"
+ssh root@"${ACCOUNT_IP}" "service docker restart"
+post_commit_content="'#!/bin/bash' \n curl -X POST http://HOSTIP:8080/job/DEMOAPP-PIPLINE/build"
+echo echo \#\!/bin/bash$'\n'"curl -X POST http://${JENKINS_IP}:${JENKINS_PORT}/job/DEMOAPP-PIPLINE/build" > $workspace/../.git/hooks/post-commit
 
 docker login -u=advantageonlineshoppingapp -p=W3lcome1
 docker stack deploy --with-registry-auth -c docker-compose.yml STACK
