903092eb7eae9e032dade36f98fe95806fb9a3ab
Michal Edery <michal.edery@hpe.com>
2017-05-09 16:40:46 +0300

    removed WORKSPACE_PATH_CALCULATED variable from aos_pipline/aos_distributed/.env & edited the post-commit path in both environments

--- Nimbus_aos/aos_pipline/aos_all_in_one/startup.sh
+++ Nimbus_aos/aos_pipline/aos_all_in_one/startup.sh
@@ -10,12 +10,17 @@ else
 fi
 command1="sed -i 's/HOST_IP_CALCULATED/$ip/g' .env"
 eval $command1
+. .env
 workspace=`pwd`
 one_level_up_workspace="${workspace%/*}"
 two_levels_up_workspace="${one_level_up_workspace%/*}"
 three_levels_up_workspace="${two_levels_up_workspace%/*}"
+workspace=${three_levels_up_workspace}
 three_levels_up_workspace=$(echo "$three_levels_up_workspace" | sed 's/\//\\\//g')
 command2="sed -i 's/WORKSPACE/${three_levels_up_workspace}\/accountservice/g' docker-compose.yml"
 eval $command2
+
+echo echo \#\!/bin/bash$'\n'"curl -X POST http://${JENKINS_IP}:${JENKINS_PORT}/job/DEMOAPP-PIPLINE/build" > $workspace/.git/hooks/post-commit
+
 docker login -u=advantageonlineshoppingapp -p=W3lcome1
 docker-compose up -d
--- Nimbus_aos/aos_pipline/aos_distributed/startup.sh
+++ Nimbus_aos/aos_pipline/aos_distributed/startup.sh
@@ -6,13 +6,14 @@ workspace=`pwd`
 one_level_up_workspace="${workspace%/*}"
 two_levels_up_workspace="${one_level_up_workspace%/*}"
 three_levels_up_workspace="${two_levels_up_workspace%/*}"
+workspace=${three_levels_up_workspace}
 three_levels_up_workspace=$(echo "$three_levels_up_workspace" | sed 's/\//\\\//g')
 command1="sed -i 's/WORKSPACE/${three_levels_up_workspace}\/accountservice/g' docker-compose.yml"
 eval $command1
 
-workspace=$(echo "$workspace" | sed 's/\//\\\//g')
-command2="sed -i 's/WORKSPACE_PATH_CALCULATED/${workspace}\//g' .env"
-eval $command2
+#workspace=$(echo "$workspace" | sed 's/\//\\\//g')
+#command2="sed -i 's/WORKSPACE_PATH_CALCULATED/${workspace}\//g' .env"
+#eval $command2
 
 sed -i "s/POSTGRES_PORT/${POSTGRES_PORT}/g" docker-compose.yml
 sed -i "s/MAIN_PORT/${MAIN_PORT}/g" docker-compose.yml
@@ -37,8 +38,7 @@ sed -i "s/TAG/${TAG}/g" docker-compose.yml
 ssh-keyscan ${ACCOUNT_IP} >> /root/.ssh/known_hosts
 echo "{ \"insecure-registries\":[\"${REGISTRY_IP}:${REGISTRY_PORT}\"] }" | ssh root@"${ACCOUNT_IP}" "cat > /etc/docker/daemon.json"
 ssh root@"${ACCOUNT_IP}" "service docker restart"
-post_commit_content="'#!/bin/bash' \n curl -X POST http://HOSTIP:8080/job/DEMOAPP-PIPLINE/build"
-echo echo \#\!/bin/bash$'\n'"curl -X POST http://${JENKINS_IP}:${JENKINS_PORT}/job/DEMOAPP-PIPLINE/build" > $workspace/../.git/hooks/post-commit
+echo echo \#\!/bin/bash$'\n'"curl -X POST http://${JENKINS_IP}:${JENKINS_PORT}/job/DEMOAPP-PIPLINE/build" > $workspace/.git/hooks/post-commit
 
 docker login -u=advantageonlineshoppingapp -p=W3lcome1
 docker stack deploy --with-registry-auth -c docker-compose.yml STACK
