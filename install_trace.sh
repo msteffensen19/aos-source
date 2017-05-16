f60146732be3359aaede978776cafa50ad956eef
Michal Edery <michal.edery@hpe.com>
2017-05-16 16:34:28 +0300

    removed shoutdown.sh command from the intasll_trace.sh

--- install_trace.sh
+++ install_trace.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 container_id=`docker ps -a | grep "performancetesting/aos-${1}-dev" | awk '{print $1}'`
 docker cp /root/HPEAppPulseJava_1.60_AdvantageOnlineShopping.zip ${container_id}:/usr/local/tomcat
-docker exec ${container_id} bash -c './bin/shutdown.sh; unzip HPEAppPulseJava_1.60_AdvantageOnlineShopping.zip; java -jar AppPulseJavaAgent/lib/AppPulseAgent.jar setup; ./bin/startup.sh'
+docker exec ${container_id} bash -c 'unzip HPEAppPulseJava_1.60_AdvantageOnlineShopping.zip; java -jar AppPulseJavaAgent/lib/AppPulseAgent.jar setup; ./bin/startup.sh'
