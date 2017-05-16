416f23d30bdd3b30bc01a572adb2863fc47652a1
Michal Edery <michal.edery@hpe.com>
2017-05-16 16:07:48 +0300

    edited install_trace.sh

--- install_trace.sh
+++ install_trace.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 container_id=`docker ps -a | grep "performancetesting/aos-${1}-dev" | awk '{print $1}'`
 docker cp /root/HPEAppPulseJava_1.60_AdvantageOnlineShopping.zip ${container_id}:/usr/local/tomcat
-docker exec -it ${container_id} bash -c './bin/shutdown.sh; unzip HPEAppPulseJava_1.60_AdvantageOnlineShopping.zip; AppPulseJavaAgent/lib; java -jar AppPulseAgent.jar setup; ./bin/startup.sh'
+docker exec -it ${container_id} bash -c './bin/shutdown.sh; unzip HPEAppPulseJava_1.60_AdvantageOnlineShopping.zip; java -jar AppPulseJavaAgent/lib/AppPulseAgent.jar setup; ./bin/startup.sh'
