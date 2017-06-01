0a1e12b5d83a7be8305dae7379f696998f571946
Michal Edery <michal.edery@hpe.com>
2017-06-01 14:43:16 +0300

    edited edit_root_pom.sh so will add a tag before each install tag in root/pom.xml

--- edit_root_pom.sh
+++ edit_root_pom.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sed -i 's/\.cmd//g' root/pom.xml
-sed -i '0,/<argument>install<\/argument>/s/<argument>install<\/argument>/<argument>--allow-root<\/argument>\n&/' root/pom.xml
+sed -i 's/<argument>install<\/argument>/<argument>--allow-root<\/argument>\n&/' root/pom.xml
