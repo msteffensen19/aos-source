7887b6aa56ab4a3206d907feb2c90bb0729de652
Michal Edery <michal.edery@hpe.com>
2017-06-21 01:55:08 +0300

    added wait-for-it.sh to the account & main dockerfile to wait until postgres is up

--- Nimbus_aos/aos_pipline/aos_distributed/startup.sh
+++ Nimbus_aos/aos_pipline/aos_distributed/startup.sh
@@ -34,18 +34,39 @@ if [ "$QUALI" == "NO" ];then
 # ip of the host
  command4="sed -i 's/PUBLIC_IP_CALCULATED/$(docker node ls | grep -w Leader | docker inspect $(awk '{print $3}') | grep -m2 "Addr" | tail -n1 | awk '{ gsub("\"",""); print $2}' | awk -F":" '{print $1}')/' .env"
  eval $command4
+ 
+ #edit .git/hooks
+ echo \#\!/bin/bash$'\n'"curl -X POST http://${JENKINS_IP}:${JENKINS_PORT}/job/DEMOAPP-PIPLINE/build" > $workspace/.git/hooks/post-commit
+ chmod +x $workspace/.git/hooks/post-commit
+
 else
+ #we assume the machins ip are already in .env
+ #change HOST_NAME to the service type, for the next command
+ list="POSTGRES MAIN ACCOUNT JENKINS JENKINS"
+ for value in $list; do sed -i "0,/HOST_NAME/s/HOST_NAME/$value/" docker-compose.yml ; done
+
+ docker node ls | grep -v Leader | grep -v HOSTNAME | awk '{print $2}' | while read line
+ do
+  public_ip=`docker inspect $line | grep "Addr" | awk '{ gsub("\"",""); print $2}'`
+  case "$public_ip" in
+   "$POSTGRES_IP") sed -i "s/POSTGRES/$line/" docker-compose.yml;;
+   "$MAIN_IP") sed -i "s/MAIN/$line/" docker-compose.yml;;
+   "$ACCOUNT_IP") sed -i "s/ACCOUNT/$line/" docker-compose.yml;;
+  esac
+ done
+ 
+ command5="sed -i 's/JENKINS/$(docker node ls | grep -w Leader | awk '{print $3}')/' docker-compose.yml"
+ eval $command5
+ 
  sed -i 's/.*WORKSPACE.*//g' docker-compose.yml
 fi
 . .env
 ssh-keyscan ${ACCOUNT_IP} >> /root/.ssh/known_hosts
 echo "{ \"insecure-registries\":[\"${REGISTRY_IP}:${REGISTRY_PORT}\"] }" | ssh root@"${ACCOUNT_IP}" "cat > /etc/docker/daemon.json"
 ssh root@"${ACCOUNT_IP}" "service docker restart"
-echo \#\!/bin/bash$'\n'"curl -X POST http://${JENKINS_IP}:${JENKINS_PORT}/job/DEMOAPP-PIPLINE/build" > $workspace/.git/hooks/post-commit
-chmod +x $workspace/.git/hooks/post-commit
 
 docker login -u=advantageonlineshoppingapp -p=W3lcome1
 docker stack deploy --with-registry-auth -c docker-compose.yml STACK
 
-command5="sed -i 's/performancetesting\/aos-accountservice.*/${REGISTRY_IP}:5000\/aos-accountservice/g' docker-compose.yml"
-eval $command5
+command6="sed -i 's/performancetesting\/aos-accountservice.*/${REGISTRY_IP}:5000\/aos-accountservice/g' docker-compose.yml"
+eval $command6
