689dff3076ce472e65bccf4139af16c964fa7b9a
kubany <fan.hot.yol-736>
2017-08-30 12:01:17 +0300

    change facebook login error message

--- catalog/src/main/java/com/advantage/catalog/store/api/CatalogController.java
+++ catalog/src/main/java/com/advantage/catalog/store/api/CatalogController.java
@@ -863,7 +863,7 @@ public class CatalogController {
         } else {
             logger.warn("cefData is null");
         }
-        ErrorResponseDto erd = new ErrorResponseDto(false, "Sorry, connection with Facebook is unavailable. Please try again later");
+        ErrorResponseDto erd = new ErrorResponseDto(false, "Sorry, connecting to Facebook is currently unavailable. Please try again later.");
         return new ResponseEntity<>(erd, HttpStatus.FORBIDDEN);
     }
     //  endregion
