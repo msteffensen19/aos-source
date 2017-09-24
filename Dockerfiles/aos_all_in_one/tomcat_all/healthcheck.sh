d997eda06aea87712439dcc638fe8aa76e05ca5e
EDERY <edery@hpe.com>
2017-09-24 17:14:26 +0300

    fixed healthcheck.sh

--- Dockerfiles/aos_all_in_one/tomcat_all/healthcheck.sh
+++ Dockerfiles/aos_all_in_one/tomcat_all/healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-status1=`curl http://${MAIN_IP}:${MAIN_PORT}/order/api/v1/healthcheck 2> /dev/null`
-if [ "$x" != "\"SUCCESS\"" ];then 
+status=`curl http://${MAIN_IP}:${MAIN_PORT}/order/api/v1/healthcheck 2> /dev/null`
+if [ "$status" != "\"SUCCESS\"" ];then 
   exit 1
 fi
