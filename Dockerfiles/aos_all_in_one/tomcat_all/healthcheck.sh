948b08a6e0a1c406e0ce865012f278196cb66c3c
EDERY <edery@hpe.com>
2017-10-01 17:17:43 +0300

    changed curl command in healthcheck.sh to localhost:8080

--- Dockerfiles/aos_all_in_one/tomcat_all/healthcheck.sh
+++ Dockerfiles/aos_all_in_one/tomcat_all/healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-status=`curl http://${MAIN_IP}:${MAIN_PORT}/order/api/v1/healthcheck 2> /dev/null`
+status=`curl http://localhost:8080/order/api/v1/healthcheck 2> /dev/null`
 while true
 do
  if [ "$status" == "\"SUCCESS\"" ];then
