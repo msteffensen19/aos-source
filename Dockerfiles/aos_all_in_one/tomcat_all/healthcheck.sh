bd8f4d7317ad008bf3d70c4d725cf42821a526df
EDERY <edery@hpe.com>
2017-10-01 17:15:09 +0300

    edited healthcheck.sh to be with a while loop

--- Dockerfiles/aos_all_in_one/tomcat_all/healthcheck.sh
+++ Dockerfiles/aos_all_in_one/tomcat_all/healthcheck.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 status=`curl http://${MAIN_IP}:${MAIN_PORT}/order/api/v1/healthcheck 2> /dev/null`
-if [ "$status" != "\"SUCCESS\"" ];then 
-  exit 1
-fi
+while true
+do
+ if [ "$status" == "\"SUCCESS\"" ];then
+  status=`curl http://localhost:8080/order/api/v1/healthcheck 2> /dev/null`
+ else
+  break
+ fi
+done
