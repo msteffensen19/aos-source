cc6c65cf6345fe31ad55b97d2ac836e400007ec5
shinat <tamir.shina@microfocus.com>
2019-06-02 19:27:23 +0300

    Coming- soon page.
    Former-commit-id: aaa6de0873a2643c65759bc082253394183238f1

--- configuration/src/landing-page/UnderConstruction.jsx
+++ configuration/src/landing-page/UnderConstruction.jsx
@@ -2,25 +2,54 @@ import React from 'react';
 import'../css-landing-page/coming-soon-css.css';
 import { Link } from 'react-router-dom';
 // noinspection ES6CheckImport
-import {ReactComponent as AdvantageLogo} from "../Advantage_logo_white.svg"
-import {ReactComponent as OvalSVG} from "../css-landing-page/Oval.svg"
-// import {ReactComponent as SoonSVG} from "../css-landing-page/Group.png"
-// import {ReactComponent as ComingSVG} from "../css-landing-page/Coming.png"
+import {ReactComponent as AdvantageLogo} from "../Advantage_logo_white.svg";
+import {ReactComponent as OvalSVG} from "../css-landing-page/Oval.svg";
+import soonPng from "../css-landing-page/Group.png";
+import comingPng from "../css-landing-page/Coming.png";
+import {ReactComponent as LeftFlag} from "../css-landing-page/left-flag.svg";
+import {ReactComponent as RightFlag} from "../css-landing-page/right-flag.svg";
+import {ReactComponent as FacebookIcon} from "../css-landing-page/Facebook.svg";
+import {ReactComponent as TwitterIcon} from "../css-landing-page/Twitter.svg";
+import {ReactComponent as LinkedinIcon} from "../css-landing-page/LinkedinIcon.svg";
+
 
 export default class UnderConstruction extends React.Component {
 
     render() {
         return (
             <Link to="/about">
-                 <div className="coming-soon">
+                 <div className="coming-soon regular-cursor">
                      <div className="advantage-header">
-                         <AdvantageLogo className="fill-3"/><h1 className= "dvantage">dvantage</h1>
+                         <AdvantageLogo className="soon-advantage-icon"/><h1 className= "dvantage more-size">dvantage</h1>
                      </div>
-                    <OvalSVG />
-                     <img src="Coming.png" alt="Italian asdfa"></img>
+                    <OvalSVG className="prevent-pointer" />
+                     <div className="images-container-comingSoon">
+                         <div>
+                             <img className="coming-soon-images coming-position regular-cursor" src={comingPng} alt="coming png"></img>
+                         </div>
+                         <div>
+                            <img className="coming-soon-images soon-position regular-cursor" src={soonPng} alt="soon png"></img>
+                         </div>
+                     </div>
+                         <div className="soon-title regular-cursor">
+                             <RightFlag />
+                                  <span className="regular-cursor">We are still working on it</span>
+                             <LeftFlag />
+                         </div>
+                     <div className="back-buttons-container">
+                             <input className="sign-in-btn button-modification" value="Back To AOS" />
+                             <button className="sign-in-btn button-modification left-space">
+                                 Back To
+                                 <br/>
+                                 Management Console
+                             </button>
+                     </div>
+                     <div className="social-links-container">
+                                <FacebookIcon />
+                                <TwitterIcon className="twitter-icon"/>
+                                <LinkedinIcon className="linkedin-icon"/>
 
-                     <img src="../css-landing-page/Group.png" alt="Italian Trulli"></img>
-
+                     </div>
                  </div>
             </Link>
         );
