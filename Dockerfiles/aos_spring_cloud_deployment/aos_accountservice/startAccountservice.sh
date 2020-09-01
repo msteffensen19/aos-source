3fedfd3ff56f6d45e986aeda88ad590ad7f67b55
akubany <assaf.kubany@microfocus.com>
2020-09-01 15:08:25 +0300

    add docker-compose config service
    Former-commit-id: 071bfa0cf53439c08ba010c0cbcddbb9a391d47a

---
+++ Dockerfiles/aos_spring_cloud_deployment/aos_accountservice/startAccountservice.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+bash wait-for-it.sh "${POSTGRES_IP}" "${POSTGRES_PORT}"
+java -jar accountservice.jar -DCONFIG_FILE_URI=${CONFIG_FILE_URI} -DACTIVE_PROFILE=${ACTIVE_PROFILE}
+tail -f /dev/null
