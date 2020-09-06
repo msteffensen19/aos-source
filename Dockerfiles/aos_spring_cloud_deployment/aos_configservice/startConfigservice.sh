5eb043d43853f4768c0e25983a166d8e03df298c
akubany <assaf.kubany@microfocus.com>
2020-09-06 12:44:54 +0300

    fix postgres waiting script
    Former-commit-id: d56437c926d0e337b067c700eeb54f1a293febf3

--- Dockerfiles/aos_spring_cloud_deployment/aos_configservice/startConfigservice.sh
+++ Dockerfiles/aos_spring_cloud_deployment/aos_configservice/startConfigservice.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-bash wait-for-it.sh "${POSTGRES_IP}" "${POSTGRES_PORT}"
+ash wait-for-it.sh "${POSTGRES_IP}" "${POSTGRES_PORT}"
 
 java -jar configservice/configservice.jar -DCONFIG_FILE_URI=${CONFIG_FILE_URI}
 tail -f /dev/null
