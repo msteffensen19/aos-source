0791646a1563cabbe4f147e2a98893f1f0d65e3a
akubany <assaf.kubany@microfocus.com>
2020-09-06 09:56:11 +0300

    fix run jar command
    Former-commit-id: 566b88b6b2fa12f14c7006262aa51d247364f856

--- Dockerfiles/aos_spring_cloud_deployment/aos_accountservice/startAccountservice.sh
+++ Dockerfiles/aos_spring_cloud_deployment/aos_accountservice/startAccountservice.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 bash wait-for-it.sh "${POSTGRES_IP}" "${POSTGRES_PORT}"
-java -jar accountservice.jar -DCONFIG_FILE_URI=${CONFIG_FILE_URI} -DACTIVE_PROFILE=${ACTIVE_PROFILE}
+java -jar accountservice/accountservice.jar -DCONFIG_FILE_URI=${CONFIG_FILE_URI} -DACTIVE_PROFILE=${ACTIVE_PROFILE}
 tail -f /dev/null
