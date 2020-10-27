76f37791aa36d62d67788130cfe6a1cfe12cc6b9
akubany <assaf.kubany@microfocus.com>
2020-10-27 08:57:40 +0200

    docker-compose dynamic service name
    Former-commit-id: ce26796f14acaa4ee34bd0cd18af82ff5a64ac1f

--- Dockerfiles/aos_spring_cloud_deployment/aos_configservice/startConfigservice.sh
+++ Dockerfiles/aos_spring_cloud_deployment/aos_configservice/startConfigservice.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 #ash wait-for-it.sh "${POSTGRES_IP}" "${POSTGRES_PORT}"
+if [ ! -f /initialized.txt ]; then # Run initialization logic
 
+    sed -i 's/ACCOUNT_SERVICE_NAME/${ACCOUNT_SERVICE_NAME}/' /microservices/application-${ACTIVE_PROFILE}.yml
+    sed -i 's/POSTGRES_SERVICE_NAME/${POSTGRES_SERVICE_NAME}/' /microservices/application-${ACTIVE_PROFILE}.yml
+    sed -i 's/MAIN_SERVICE_NAME/${MAIN_SERVICE_NAME}/' /microservices/application-${ACTIVE_PROFILE}.yml
+    touch /initialized.txt
+fi
 java -jar configservice/configservice.jar -DCONFIG_FILE_URI=${CONFIG_FILE_URI}
 tail -f /dev/null
