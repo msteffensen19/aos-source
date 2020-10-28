e485828c419be0b15b907a084f67f5aa9522f6c6
akubany <assaf.kubany@microfocus.com>
2020-10-28 09:16:52 +0200

    fix gateway configuration
    Former-commit-id: 828a46f2da291fa288a88e6fdebfc751a31a8d43

--- Dockerfiles/aos_spring_cloud_deployment/aos_configservice/startConfigservice.sh
+++ Dockerfiles/aos_spring_cloud_deployment/aos_configservice/startConfigservice.sh
@@ -4,6 +4,8 @@ if [ ! -f /initialized.txt ]; then # Run initialization logic
     sed -i "s/ACCOUNT_SERVICE_NAME/${ACCOUNT_SERVICE_NAME}/g" configservice/microservices/application-${ACTIVE_PROFILE}.yml
     sed -i "s/POSTGRES_SERVICE_NAME/${POSTGRES_SERVICE_NAME}/g" configservice/microservices/application-${ACTIVE_PROFILE}.yml
     sed -i "s/MAIN_SERVICE_NAME/${MAIN_SERVICE_NAME}/g" configservice/microservices/application-${ACTIVE_PROFILE}.yml
+    sed -i "s/MAIN_SERVICE_NAME/${MAIN_SERVICE_NAME}/g" configservice/microservices/aos-gateway-${ACTIVE_PROFILE}.yml
+    sed -i "s/ACCOUNT_SERVICE_NAME/${ACCOUNT_SERVICE_NAME}/g" configservice/microservices/aos-gateway-${ACTIVE_PROFILE}.yml
     touch /initialized.txt
 fi
 java -jar configservice/configservice.jar -DCONFIG_FILE_URI=${CONFIG_FILE_URI}
